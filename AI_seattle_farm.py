bbb1928d79a23b331b486e59d4504cacdb4c8d54
ArthurS <sarronarthur@gmail.com>
2024-03-05 14:19:05 +0100

    Updating code

--- AI_seattle_farm.py
+++ AI_seattle_farm.py
@@ -1,9 +1,12 @@
 import pandas
 import wget
 
-wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/m0b_optimizer.py
-wget http://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/seattleWeather_1948-2017.csv
+url1='https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/m0b_optimizer.py'
+filename= wget.download(url1)
+filename
+##!wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/m0b_optimizer.py
+##!wget https://https://github.com/Arthur-Sarron/Ai_formation/tree/main/Data/seattleWeather1948-2017.csv
 
 
-data= pandas.read_csv('seattleWeather_1948-2017.csv', parse_dates=['date'])
-data=data[[d.month == 1 for d in data.date]].copy()
+#data= pandas.read_csv('seattleWeather_1948-2017.csv', parse_dates=['date'])
+#data=data[[d.month == 1 for d in data.date]].copy()
